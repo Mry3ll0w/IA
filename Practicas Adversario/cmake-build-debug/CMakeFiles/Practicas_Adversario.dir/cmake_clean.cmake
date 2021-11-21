96b43c11d3cc57a4164c7d021d570e2f195c6600
Antonio Roldan Andrade <antonioroldanandrade@gmail.com>
2021-11-21 01:40:37 +0100

    Makefile para Adversarios

---
+++ Practicas Adversario/cmake-build-debug/CMakeFiles/Practicas_Adversario.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practicas_Adversario.dir/main.c.obj"
+  "CMakeFiles/Practicas_Adversario.dir/minimaxAlum.c.obj"
+  "CMakeFiles/Practicas_Adversario.dir/tictactoe.c.obj"
+  "Practicas_Adversario.exe"
+  "Practicas_Adversario.exe.manifest"
+  "Practicas_Adversario.pdb"
+  "libPracticas_Adversario.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Practicas_Adversario.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
